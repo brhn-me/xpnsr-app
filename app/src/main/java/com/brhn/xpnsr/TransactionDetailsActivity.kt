f4d61ec3a3061648930ce68468f4272c9ef2d741
burhan <burhan.bd@gmail.com>
2024-01-22 18:12:34 +0200

    - added transaction icons - added sample transactions - added transaction details - added on list click to details button action

---
+++ app/src/main/java/com/brhn/xpnsr/TransactionDetailsActivity.kt
@@ -0,0 +1,54 @@
+package com.brhn.xpnsr
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.brhn.xpnsr.ui.theme.XPNSRTheme
+
+class TransactionDetailActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val transaction = intent.getSerializableExtra("TRANSACTION_DATA") as Transaction
+
+        setContent {
+            XPNSRTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    TransactionDetail(transaction)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TransactionDetail(transaction: Transaction) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        
+        Text(
+            text = "Transaction Details",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text("Name: ${transaction.name}", fontSize = 18.sp)
+        Text("Date: ${transaction.date}", fontSize = 18.sp)
+        Text("Amount: \$${transaction.amount}", fontSize = 18.sp)
+        Text("Type: ${transaction.type}", fontSize = 18.sp)
+        Text("Category: ${transaction.category}", fontSize = 18.sp)
+    }
+}
--- app/src/main/java/com/brhn/xpnsr/TransactionItem.kt
+++ app/src/main/java/com/brhn/xpnsr/TransactionItem.kt
@@ -1,28 +1,68 @@
 package com.brhn.xpnsr
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
+
 @Composable
-fun TransactionItem(transaction: Transaction) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
+fun TransactionItem(transaction: Transaction, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(all = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background), // Replace with your actual image resource
+            painter = painterResource(id = getCategoryIcon(transaction.category)),
             contentDescription = "Transaction Image",
-            modifier = Modifier.padding(end = 8.dp)
+            modifier = Modifier
+                .size(40.dp)
+                .padding(end = 8.dp)
         )
-        Column {
-            Text(text = transaction.name)
-            Text(text = transaction.date)
-            Text(text = "Amount: \$${transaction.amount}")
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 8.dp)
+        ) {
+            Text(
+                text = transaction.name,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = transaction.category,
+                color = Color.Gray
+            )
+        }
+
+        Column(
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(
+                text = "\$${String.format("%.2f", transaction.amount)}",
+                color = if (transaction.type == TransactionType.EXPENSE) Color.Red else Color.Green,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = transaction.date,
+                color = Color.Gray
+            )
         }
     }
 }
