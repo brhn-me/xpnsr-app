96ef050a52668d2ea45e58b0d3454e5630e1633e
burhan <burhan.bd@gmail.com>
2024-01-29 22:08:08 +0200

    - removed unnecessary MainActivity.kt

--- app/src/main/java/com/brhn/xpnsr/TransactionListActivity.kt
+++ app/src/main/java/com/brhn/xpnsr/TransactionListActivity.kt
@@ -5,9 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import java.io.Serializable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -17,8 +22,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.brhn.xpnsr.ui.theme.XPNSRTheme
 
 class TransactionListActivity : ComponentActivity() {
@@ -152,4 +163,52 @@ data class Transaction(
     val type: TransactionType,
     val category: String
 ) : Serializable
+
+@Composable
+fun TransactionItem(transaction: Transaction, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(all = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = getCategoryIcon(transaction.category)),
+            contentDescription = "Transaction Image",
+            modifier = Modifier
+                .size(40.dp)
+                .padding(end = 8.dp)
+        )
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 8.dp)
+        ) {
+            Text(
+                text = transaction.name,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = transaction.category,
+                color = Color.Gray
+            )
+        }
+
+        Column(
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(
+                text = "\$${String.format("%.2f", transaction.amount)}",
+                color = if (transaction.type == TransactionType.EXPENSE) Color.Red else Color.Green,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = transaction.date,
+                color = Color.Gray
+            )
+        }
+    }
+}
 
